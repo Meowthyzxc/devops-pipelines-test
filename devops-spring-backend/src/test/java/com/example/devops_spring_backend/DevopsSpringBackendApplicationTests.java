699b0cf8c86d0bb3b8a798ee965cd87030d010b3
mwt.dev <it.michaelobligar@gmail.com>
2025-11-29 03:39:07 +0000

    Initial commit of Spring Boot app

---
+++ devops-spring-backend/src/test/java/com/example/devops_spring_backend/DevopsSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.devops_spring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsSpringBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
